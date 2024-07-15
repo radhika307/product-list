74b8c309c22d39811ec8511db94875fd20bac36a
Radhika <rradhika393@gmail.com>
2024-07-15 22:01:21 +0530

    Configured tailwind CSS.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="bg-blue-600">
+      <h2 className="text-white text-2xl font-bold p-4 text-center">
+        Welcome to products page.
+      </h2>
+    </div>
+  );
 };
 
 export default Header;
--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -3,13 +3,13 @@ import React from "react";
 const ProductCard = (product) => {
   return (
     <div>
-      <p>{product.props.title}</p>
       <img
         alt="product-image"
         src={product.props.images[0]}
         width={200}
         height={200}
       />
+      <p className="text-blue-600">{product.props.title}</p>
     </div>
   );
 };
